a960f5de6ccc906246425238ad51495ea38be436
Ivan Suhih <i.suhih@di-group.biz>
2017-10-17 18:57:41 +0700

    configure awaits to avoid possible deadlock

--- NunitParallelTest/TestTest.cs
+++ NunitParallelTest/TestTest.cs
@@ -13,61 +13,61 @@ namespace Timetracker.Tests.DesktopConsoleRunner
         [Test, Parallelizable]
         public async Task Test1()
         {
-            await Task.Delay(1000);
+            await Task.Delay(1000).ConfigureAwait(false);
             Assert.IsTrue(true);
         }
         [Test, Parallelizable]
         public async Task Test2()
         {
-            await Task.Delay(1200);
+            await Task.Delay(1200).ConfigureAwait(false);
             Assert.IsTrue(true);
         }
         [Test, Parallelizable]
         public async Task Test3()
         {
-            await Task.Delay(1300);
+            await Task.Delay(1300).ConfigureAwait(false);
             Assert.IsTrue(true);
         }
         [Test, Parallelizable]
         public async Task Test4()
         {
-            await Task.Delay(1400);
+            await Task.Delay(1400).ConfigureAwait(false);
             Assert.IsTrue(true);
         }
         [Test, Parallelizable]
         public async Task Test5()
         {
-            await Task.Delay(1500);
+            await Task.Delay(1500).ConfigureAwait(false);
             Assert.IsTrue(true);
         }
         [Test, Parallelizable]
         public async Task Test6()
         {
-            await Task.Delay(600);
+            await Task.Delay(600).ConfigureAwait(false);
             Assert.IsTrue(true);
         }
         [Test, Parallelizable]
         public async Task Test71()
         {
-            await Task.Delay(700);
+            await Task.Delay(700).ConfigureAwait(false);
             Assert.IsTrue(true);
         }
         [Test, Parallelizable]
         public async Task Test8()
         {
-            await Task.Delay(800);
+            await Task.Delay(800).ConfigureAwait(false);
             Assert.IsTrue(true);
         }
         [Test, Parallelizable]
         public async Task Test9()
         {
-            await Task.Delay(900);
+            await Task.Delay(900).ConfigureAwait(false);
             Assert.IsTrue(true);
         }
         [Test, Parallelizable]
         public async Task Test11()
         {
-            await Task.Delay(1000);
+            await Task.Delay(1000).ConfigureAwait(false);
             Assert.IsTrue(true);
         }
     }

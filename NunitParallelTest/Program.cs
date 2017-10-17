a653444c895f270bcc43373a0636309432f3e43b
Ivan Suhih <i.suhih@di-group.biz>
2017-10-17 18:46:23 +0700

    init

---
+++ NunitParallelTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitParallelTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ NunitParallelTest/TestTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Timetracker.Tests.DesktopConsoleRunner
+{
+    [TestFixture]
+    public class TestTest
+    {
+        [Test, Parallelizable]
+        public async Task Test1()
+        {
+            await Task.Delay(1000);
+            Assert.IsTrue(true);
+        }
+        [Test, Parallelizable]
+        public async Task Test2()
+        {
+            await Task.Delay(1200);
+            Assert.IsTrue(true);
+        }
+        [Test, Parallelizable]
+        public async Task Test3()
+        {
+            await Task.Delay(1300);
+            Assert.IsTrue(true);
+        }
+        [Test, Parallelizable]
+        public async Task Test4()
+        {
+            await Task.Delay(1400);
+            Assert.IsTrue(true);
+        }
+        [Test, Parallelizable]
+        public async Task Test5()
+        {
+            await Task.Delay(1500);
+            Assert.IsTrue(true);
+        }
+        [Test, Parallelizable]
+        public async Task Test6()
+        {
+            await Task.Delay(600);
+            Assert.IsTrue(true);
+        }
+        [Test, Parallelizable]
+        public async Task Test71()
+        {
+            await Task.Delay(700);
+            Assert.IsTrue(true);
+        }
+        [Test, Parallelizable]
+        public async Task Test8()
+        {
+            await Task.Delay(800);
+            Assert.IsTrue(true);
+        }
+        [Test, Parallelizable]
+        public async Task Test9()
+        {
+            await Task.Delay(900);
+            Assert.IsTrue(true);
+        }
+        [Test, Parallelizable]
+        public async Task Test11()
+        {
+            await Task.Delay(1000);
+            Assert.IsTrue(true);
+        }
+    }
+}
